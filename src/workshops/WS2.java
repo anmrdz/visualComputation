daed3072b748166affb7348a0589137e5d6176ae
Rockero <Rockero@laptopRockero>
2016-08-20 19:46:18 -0500

    Ya esta implementado la base del taller 2

--- src/workshops/WS2.java
+++ src/workshops/WS2.java
@@ -1,54 +1,107 @@
 package workshops;
 
-import processing.core.*;
 import processing.core.PApplet;
+import static processing.core.PConstants.CENTER;
+import static processing.core.PConstants.LEFT;
+import static processing.core.PConstants.P2D;
+import processing.core.PGraphics;
 import processing.core.PImage;
+import processing.event.MouseEvent;
 
 public class WS2 extends PApplet {
 
-    PImage img, img2;
-    PGraphics pg, secondLy;
+	float ancho = 321, alto = 204, grosor = 5, escala = 1, ancho2 = 321, alto2 = 204, angle = 0;
 
-    int x0 = 0, y0 = 0;
+	PGraphics detalle, completo;
+	PImage ImgC, ImgD;
 
-    public void setup() {
-        img = loadImage("resources/w1/map_copia.png");
-        img2 = loadImage("resources/w1/map.png");
-        pg = createGraphics(440, 300);
-        secondLy = createGraphics(1000, 600);
-       
-        secondLy.beginDraw();
-        secondLy.image(img2, 0, 0);
-        secondLy.endDraw();
-        pg.beginDraw();
-        pg.image(img, 0, 0);
-        pg.endDraw();
-        
-        
-    }
+	public static void main(String[] args) {
+		PApplet.main(new String[] { "workshops.WS2" });
+	}
 
-    public void settings() {
-        size(1200, 800);
-    }
+	@Override
+	public void settings() {
+		size((int) ancho * 3, (int) alto * 3, P2D);
+	}
 
-    public void draw() {
+	@Override
+	public void setup() {
+		completo = createGraphics((int) ancho * 3, (int) alto * 3);
+		detalle = createGraphics((int) ancho, (int) alto);
+	}
 
-        background(0);
-        
-        image(secondLy, 0, 0);
-        image(pg, 0, 0);
+	@Override
+	public void draw() {
+		if (ancho * (0.5f * escala) > 0) {
+			ancho2 = ancho * (0.5f * escala);
+		} else {
+			ancho2 = ancho;
+			escala = 1;
+		}
+		if (alto * (0.5f * escala) > 0) {
+			alto2 = alto * (0.5f * escala);
+		} else {
+			alto2 = alto;
+			escala = 1;
+		}
 
-    }
+		background(0);
 
-    public void mousePressed() {
-        
-    }
+		ImgD = loadImage("resources/w2/NYC.jpg");
+		ImgD.resize((int) ancho, (int) alto);
+		image(ImgD, 0, 0);
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        PApplet.main(new String[]{workshops.WS2.class.getName()});
-    }
+		ImgC = get((int) (mouseX - (ancho2 / 20) - (grosor / 2)), (int) (mouseY - (alto2 / 20) - (grosor / 2)),
+				(int) ((ancho2 / 10) + grosor), (int) ((alto2 / 10) + grosor));
+		ImgC.resize((int) ancho * 3, (int) alto * 3);
 
+		pushMatrix();
+		completo.beginDraw();
+		completo.background(0);
+
+		pushMatrix();
+		completo.translate((ancho * 3) / 2, (alto * 3) / 2);
+		completo.rotate(angle);
+		completo.imageMode(CENTER);
+		completo.image(ImgC, 0, 0);
+		popMatrix();
+
+		completo.endDraw();
+		popMatrix();
+
+		image(completo, 0, 0);
+
+		pushMatrix();
+		detalle.beginDraw();
+		detalle.background(0);
+		detalle.image(ImgD, 0, 0);
+		detalle.noFill();
+		detalle.stroke(255);
+		detalle.strokeWeight(grosor);
+
+		pushMatrix();
+		detalle.translate(mouseX, mouseY);
+		// detalle.rotate(angle);
+		detalle.rectMode(CENTER);
+		detalle.rect(0, 0, (ancho2 / 10) + grosor, (alto2 / 10) + grosor);
+		popMatrix();
+
+		detalle.endDraw();
+		popMatrix();
+
+		image(detalle, 0, 0);
+
+		if (mousePressed) {
+			if (mouseButton == LEFT) {
+				angle -= 0.1f;
+			} else {
+				angle += 0.1f;
+			}
+		}
+	}
+
+	@Override
+	public void mouseWheel(MouseEvent event) {
+		escala += event.getCount();
+	}
 }
