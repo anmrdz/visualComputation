750d79310a9c4e4ab6a31bfbf6678b4d30f87d88
anmrdz <anmrdz@gmail.com>
2016-09-07 23:35:28 -0500

    workshop 2 correct coordinates and zooming

--- src/workshops/WS2.java
+++ src/workshops/WS2.java
@@ -1,8 +1,6 @@
 package workshops;
 
 import processing.core.PApplet;
-import static processing.core.PConstants.CENTER;
-import static processing.core.PConstants.LEFT;
 import static processing.core.PConstants.P2D;
 import processing.core.PGraphics;
 import processing.core.PImage;
@@ -10,98 +8,82 @@ import processing.event.MouseEvent;
 
 public class WS2 extends PApplet {
 
-	float ancho = 321, alto = 204, grosor = 5, escala = 1, ancho2 = 321, alto2 = 204, angle = 0;
+    float scale = 1.0f, angle = 0;
+    int visorSize = 50, sectionSize = 300, miniMapSize;
 
-	PGraphics detalle, completo;
-	PImage ImgC, ImgD;
+    PGraphics minimap, map;
+    PImage imgMap, minimg;
 
-	public static void main(String[] args) {
-		PApplet.main(new String[] { "workshops.WS2" });
-	}
+    @Override
+    public void settings() {
+        size(750, 750, P2D);
+    }
 
-	@Override
-	public void settings() {
-		size((int) ancho * 3, (int) alto * 3, P2D);
-	}
+    @Override
+    public void setup() {
+        miniMapSize = width / 3;
+        map = createGraphics(width, height);
+        minimap = createGraphics(miniMapSize, miniMapSize);
+        imgMap = loadImage("resources/w2/map.jpg");
+        minimg = imgMap.copy();
+        minimg.resize(miniMapSize, miniMapSize);
+    }
 
-	@Override
-	public void setup() {
-		completo = createGraphics((int) ancho * 3, (int) alto * 3);
-		detalle = createGraphics((int) ancho, (int) alto);
-	}
+    @Override
+    public void draw() {
+        background(0);
+        noCursor();
+        int visorCurrentSize =(int) (scale * visorSize);
+        int sectionCurrentSize = (int) (scale * sectionSize);
+        
+        mouseX = constrain(mouseX, 0, miniMapSize - visorCurrentSize);
+        mouseY = constrain(mouseY, 0, miniMapSize - visorCurrentSize);
+        
+        int posX = mouseX * 6, posY = mouseY * 6;
+        
+        minimap.beginDraw();
+        minimap.image(minimg, 0, 0);
+        minimap.noFill();
+        minimap.stroke(255);
+        minimap.pushMatrix();
+        minimap.translate(mouseX + visorCurrentSize/2, mouseY + visorCurrentSize/2);
+        minimap.rotate(radians(angle));
+        System.out.println("angle " + angle);
+        minimap.rect(-visorCurrentSize/2, -visorCurrentSize/2, visorCurrentSize, visorCurrentSize);
+        minimap.popMatrix();
+        minimap.endDraw();
 
-	@Override
-	public void draw() {
-		if (ancho * (0.5f * escala) > 0) {
-			ancho2 = ancho * (0.5f * escala);
-		} else {
-			ancho2 = ancho;
-			escala = 1;
-		}
-		if (alto * (0.5f * escala) > 0) {
-			alto2 = alto * (0.5f * escala);
-		} else {
-			alto2 = alto;
-			escala = 1;
-		}
+        PImage section = imgMap.get(posX, posY, sectionCurrentSize, sectionCurrentSize);
+      
+        map.beginDraw();
+        section.resize(width, height);
+        map.image(section, 0, 0);
+        map.endDraw();
 
-		background(0);
+        image(map, 0, 0);
+        image(minimap, 0, 0);
+    }
 
-		ImgD = loadImage("resources/w2/NYC.jpg");
-		ImgD.resize((int) ancho, (int) alto);
-		image(ImgD, 0, 0);
+    @Override
+    public void mouseWheel(MouseEvent event) {
+        scale += (event.getCount() / 2.0);
+        if (scale < 0.5) {
+            scale = 0.5f;
+        } else if (scale > 5.0) {
+            scale = 5.0f;
+        }
+    }
 
-		ImgC = get((int) (mouseX - (ancho2 / 20) - (grosor / 2)), (int) (mouseY - (alto2 / 20) - (grosor / 2)),
-				(int) ((ancho2 / 10) + grosor), (int) ((alto2 / 10) + grosor));
-		ImgC.resize((int) ancho * 3, (int) alto * 3);
+    @Override
+    public void mousePressed() {
+        if (mouseButton == LEFT) {
+            angle -= 10;
+        } else if (mouseButton == RIGHT) {
+            angle += 10;
+        }
+    }
 
-		pushMatrix();
-		completo.beginDraw();
-		completo.background(0);
-
-		pushMatrix();
-		completo.translate((ancho * 3) / 2, (alto * 3) / 2);
-		completo.rotate(angle);
-		completo.imageMode(CENTER);
-		completo.image(ImgC, 0, 0);
-		popMatrix();
-
-		completo.endDraw();
-		popMatrix();
-
-		image(completo, 0, 0);
-
-		pushMatrix();
-		detalle.beginDraw();
-		detalle.background(0);
-		detalle.image(ImgD, 0, 0);
-		detalle.noFill();
-		detalle.stroke(255);
-		detalle.strokeWeight(grosor);
-
-		pushMatrix();
-		detalle.translate(mouseX, mouseY);
-		// detalle.rotate(angle);
-		detalle.rectMode(CENTER);
-		detalle.rect(0, 0, (ancho2 / 10) + grosor, (alto2 / 10) + grosor);
-		popMatrix();
-
-		detalle.endDraw();
-		popMatrix();
-
-		image(detalle, 0, 0);
-
-		if (mousePressed) {
-			if (mouseButton == LEFT) {
-				angle -= 0.1f;
-			} else {
-				angle += 0.1f;
-			}
-		}
-	}
-
-	@Override
-	public void mouseWheel(MouseEvent event) {
-		escala += event.getCount();
-	}
+    public static void main(String[] args) {
+        PApplet.main(new String[]{workshops.WS2.class.getName()});
+    }
 }
